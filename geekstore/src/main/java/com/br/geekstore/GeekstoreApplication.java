a4d1943080be4a33d00bca8346095539af62bee7
Jeferson Amorim <jeferson.amorim@live.com>
2021-10-16 10:36:51 -0300

    Definindo Arquitetura do Projeto

---
+++ geekstore/src/main/java/com/br/geekstore/GeekstoreApplication.java
@@ -0,0 +1,13 @@
+package com.br.geekstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeekstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeekstoreApplication.class, args);
+	}
+
+}
---
+++ geekstore/src/test/java/com/br/geekstore/GeekstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.geekstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeekstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
